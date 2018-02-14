a3ecc6216fbc0fb9de30b73199c5d03a7eb47bab
rhondarobot <rrachellej@gmail.com>
2018-02-13 23:34:47 -0700

    timer working and positioned via bounds next to icon

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+const path = require('path');
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false // so the timer won't show until the tray icon is pressed
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png': 'iconTemplate.png';
+    const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
+    tray = new Tray(iconPath);
+    tray.on('click', (event, bounds) => {
+        // click event bounds
+        const { x, y } = bounds;
+
+        // window width and height
+        const { width, height } = mainWindow.getBounds();
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                width,
+                height
+            });
+            mainWindow.show();
+        }    
+    });
 });
